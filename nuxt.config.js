80e825f1454fdf221cf513ac4740830aa5aa7a06
Christiaen François <christiaen.francois@gmail.com>
2020-12-24 11:49:50 +0100

    remove locomotive

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: ['~/assets/scss/layout.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~plugins/locomotive.client'],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
